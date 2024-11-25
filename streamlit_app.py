0adee80f9df7230e4c33f42516babafde4178205
rebeccamadeit <nrbucher@gmail.com>
2024-11-25 19:25:21 +0000

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import datetime
 
-st.title("🎈 Customize My Christmas Tree")
+st.title(":evergreen_tree: Customize My Christmas Tree")
 st.write(
     "Which Lights do you like! Vote for your favorite below."
 )
@@ -9,6 +9,10 @@ st.write(
 with open("whitefile.txt", "r") as f:
     a = f.readline()  # starts as a string
     a = 0 if a == "" else int(a)  # check if its an empty string, otherwise should be able to cast using int()
+
+with open("multifile.txt", "r") as f2:
+    b = f2.readline()  # starts as a string
+    b = 0 if b == "" else int(b)  # check if its an empty string, otherwise should be able to cast using int()
 
 if st.button("Vote for all White Lights"):
     a += 1  
@@ -20,5 +24,18 @@ if st.button("Vote for all White Lights"):
             "Thanks, your vote was collected on: " + str(today)
 )
 
+if st.button("Vote for Multi-Color Lights"):
+    b += 1  
+    with open("multifile.txt", "w") as f2:
+        f2.truncate()
+        f2.write(f"{b}")
+        today = datetime.datetime.now()
+        st.write(
+            "Thanks, your vote was collected on: " + str(today)
+)
+        
+st.write("Current White Light Count is: ")
 a
+st.write("Current Multi-Color Light Count is: ")
+b
 
