0c8c1d441dfbc3af1e013fdbf5a0819cf2c5fdc9
rebeccamadeit <nrbucher@gmail.com>
2024-11-25 17:35:14 +0000

    add second button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import datetime
 
-st.title("🎈 My new app")
+st.title("🎈 Customize My Christmas Tree")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Which Lights do you like! Vote for your favorite below."
 )
+
+with open("whitefile.txt", "r") as f:
+    a = f.readline()  # starts as a string
+    a = 0 if a == "" else int(a)  # check if its an empty string, otherwise should be able to cast using int()
+
+if st.button("Vote for all White Lights"):
+    a += 1  
+    with open("whitefile.txt", "w") as f:
+        f.truncate()
+        f.write(f"{a}")
+        today = datetime.datetime.now()
+        st.write(
+            "Thanks, your vote was collected on: " + str(today)
+)
+
+a
+
